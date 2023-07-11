98114a406a78bd79e443b9e60aeddc63f1fc1fca
Mojala Charit Vinay <Mojala.V@Youngsoftindia.local>
2023-07-11 11:33:55 +0530

    first commit

---
+++ src/test/java/com/ecommerce/Ecommerce/Application/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Ecommerce.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
